ab3328d907ca32e7f21af198caf18a050a06a69e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-21 14:59:14 -0700

    Create test for older than me challenge

---
+++ challenges/older-than-me.js
@@ -0,0 +1,26 @@
+// Older Than Me
+// Create a method in the Person class which returns how another person's 
+// age compares. Given the instances p1, p2 and p3, which will be initialized 
+// with the attributes name and age, return a sentence in the following format:
+
+// {other person name} is {older than / younger than / the same age as} me.
+
+// Examples
+// p1 = Person("Samuel", 24)
+// p2 = Person("Joel", 36)
+// p3 = Person("Lily", 24)
+// p1.compareAge(p2) ➞ "Joel is older than me."
+
+// p2.compareAge(p1) ➞ "Samuel is younger than me."
+
+// p1.compareAge(p3) ➞ "Lily is the same age as me."
+// Notes
+
+// Check out the Resources tab for some helpful tutorials on JavaScript classes!
+// If you're really stuck, check out the Solutions tab for answers.
+
+class Person {
+  
+}
+
+module.exports = Person;
---
+++ test/older-than-me-test.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var compareAge = require('../challenges/older-than-me');
+
+describe('compareAge', function() {
+
+  it('should return a string stating who is the older of two people', function() {
+    var p1 = new Person("Samuel", 24);
+    var p2 = new Person("Joel", 36);
+    var p3 = new Person("Lily", 24);
+    assert.equal(p1.compareAge(p2), "Joel is older than me.");
+    assert.equal(p1.compareAge(p3), "Lily is the same age as me.");
+    
+    assert.equal(p2.compareAge(p1), "Samuel is younger than me.");
+    assert.equal(p2.compareAge(p3), "Lily is younger than me.");
+
+    assert.equal(p3.compareAge(p1), "Samuel is the same age as me.");
+    assert.equal(p3.compareAge(p2), "Joel is older than me.");
+  });
+});
