4d7314661e7618d190d06e72e267704c9b0f47d3
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-23 13:25:57 -0700

    Refactor drink sorting solution to single line

--- challenges/drink-sorting.js
+++ challenges/drink-sorting.js
@@ -19,8 +19,7 @@
 // N/A
 
 function sortDrinkByPrice(drinks) {
-	drinks.sort((a, b) => a.price-b.price);
-  return drinks;
+	return drinks.sort((a, b) => a.price-b.price);
 }
 
 module.exports = sortDrinkByPrice
