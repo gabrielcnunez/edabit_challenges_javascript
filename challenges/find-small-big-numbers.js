525d222a716f17698bbe659912b5369b066babf5
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-06-26 16:17:04 -0700

    Create test for find smallest and biggest number challenge

---
+++ challenges/find-small-big-numbers.js
@@ -0,0 +1,19 @@
+// Find the Smallest and Biggest Numbers
+// Create a function that takes an array of numbers and return 
+// both the minimum and maximum numbers, in that order.
+
+// Examples
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+// minMax([2334454, 5]) ➞ [5, 2334454]
+
+// minMax([1]) ➞ [1, 1]
+
+// Notes
+// All test arrays will have at least one element and are valid.
+
+function minMax(arr) {
+
+}
+
+module.exports = minMax;
