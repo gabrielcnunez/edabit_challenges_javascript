72a8560a26648c5692df9b66f56f5648afe64d45
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-26 16:13:16 -0700

    Create test for find the amount of potatoes challenge

---
+++ challenges/amount-of-potatoes.js
@@ -0,0 +1,18 @@
+// Find the Amount of Potatoes
+// Create a function to return the amount of potatoes there are in a string.
+
+// Examples
+// potatoes("potato") ➞ 1
+
+// potatoes("potatopotato") ➞ 2
+
+// potatoes("potatoapple") ➞ 1
+
+// Notes
+// N/A
+
+function potatoes(str) {
+
+}
+
+module.exports = potatoes
---
+++ test/amount-of-potatoes-test.js
@@ -0,0 +1,14 @@
+var assert = require('chai').assert
+var potatoes = require('../challenges/amount-of-potatoes')
+
+describe('Amount of potatoes', function() {
+
+  it('counts the number of times potato appears in a string', function() {
+    assert.equal(potatoes("potato"), 1)
+    assert.equal(potatoes("potatopotatocherry"), 2)
+    assert.equal(potatoes("potatopotatopotatoorange"), 3)
+    assert.equal(potatoes("potatopotatobananapotatopotato"), 4)
+    assert.equal(potatoes("potatopotatomangopotatopotatopotato"), 5)
+    assert.equal(potatoes("potatocucumberpotatopotatopotatopotatopotato"), 6)
+  })
+})
