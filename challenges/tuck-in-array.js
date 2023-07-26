30d0d497dc52cc9425de26a5a4a0ab70af71619e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-25 22:11:20 -0700

    Create test for tuck in challenge

---
+++ challenges/tuck-in-array.js
@@ -0,0 +1,20 @@
+// Tuck in Array
+// Create a function that takes two arrays and insert the 
+// second array in the middle of the first array.
+
+// Examples
+// tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]) ➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// tuckIn([15,150], [45, 75, 35]) ➞ [15, 45, 75, 35, 150]
+
+// tuckIn([[1, 2], [5, 6]], [[3, 4]]) ➞ [[1, 2], [3, 4], [5, 6]]
+
+// Notes
+// The first array always has two elements.
+// Use the spread syntax to solve this challenge.
+
+function tuckIn(arr1, arr2) {
+
+}
+
+module.exports = tuckIn;
