d1d0180a7cf9acc180627e141ddb92ae290e2aca
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-27 13:51:52 -0700

    Create test for which generation are you challenge

---
+++ challenges/which-generation.js
@@ -0,0 +1,33 @@
+// Which Generation Are You?
+// Try finding your ancestors and offspring with code.
+
+// Create a function that takes a number x and a character y ("m" for male, "f" for female), 
+// and returns the name of an ancestor (m/f) or descendant (m/f).
+
+// If the number is negative, return the related ancestor.
+// If positive, return the related descendant.
+// You are generation 0. In the case of 0 (male or female), return "me!".
+// Examples
+// generation(2, "f") ➞ "granddaughter"
+
+// generation(-3, "m") ➞ "great grandfather"
+
+// generation(1, "f") ➞ "daughter"
+
+// Notes
+// Check the Resources tab for helpful hints.
+
+// Generation	Male	Female
+// -3	great grandfather	great grandmother
+// -2	grandfather	grandmother
+// -1	father	mother
+// 0	me!	me!
+// 1	son	daughter
+// 2	grandson	granddaughter
+// 3	great grandson	great granddaughter
+
+function generation(x, y) {
+
+}
+
+module.exports = generation;
---
+++ test/which-generation-test.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert
+var generation = require('../challenges/which-generation')
+
+describe('Which Generation Are You?', function() {
+
+  it('returns the name of the ancestor/descendant based on a number and character', function() {
+    assert.equal(generation(-3, "m"), "great grandfather");
+    assert.equal(generation(1, "f"), "daughter");
+    assert.equal(generation(-3, "f"), "great grandmother");
+    assert.equal(generation(-2, "m"), "grandfather");
+    assert.equal(generation(-2, "f"), "grandmother");
+    assert.equal(generation(-1, "m"), "father");
+    assert.equal(generation(-1, "f"), "mother");
+    assert.equal(generation(0, "f"), "me!");
+    assert.equal(generation(1, "m"), "son");
+    assert.equal(generation(1, "f"), "daughter");
+    assert.equal(generation(2, "m"), "grandson");
+    assert.equal(generation(2, "f"), "granddaughter");
+    assert.equal(generation(3, "m"), "great grandson");
+    assert.equal(generation(3, "f"), "great granddaughter");
+    assert.equal(generation(0, "m"), "me!");
+  });
+});
