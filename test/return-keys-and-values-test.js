f27c91b10ba992e9b1f46cbec9aa5333bb6a0a72
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-05 15:21:05 -0700

    Uncomment skipped tests, method still passes

--- test/return-keys-and-values-test.js
+++ test/return-keys-and-values-test.js
@@ -5,9 +5,9 @@ describe('Return the Object Keys and Values', function() {
 
   it('takes an object and return keys and values as arrays', function() {
     assert.deepEqual(keysAndValues({a: 1, b: 2, c: 3}), [["a", "b", "c"], [1, 2, 3]]);
-    // assert.equal(keysAndValues({a: "Apple", b: "Microsoft", c: "Google"}), [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]);
-    // assert.equal(keysAndValues({key1: true, key2: false, key3: undefined}), [["key1", "key2", "key3"], [true, false, undefined]]);
-    // assert.equal(keysAndValues({1: null, 2: null, 3: null}), [["1", "2", "3"], [null, null, null]]);
-    // assert.equal(keysAndValues({key1: "cat", key2: "dog", key3: null}), [["key1", "key2", "key3"], ["cat", "dog", null]]);
+    assert.deepEqual(keysAndValues({a: "Apple", b: "Microsoft", c: "Google"}), [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]);
+    assert.deepEqual(keysAndValues({key1: true, key2: false, key3: undefined}), [["key1", "key2", "key3"], [true, false, undefined]]);
+    assert.deepEqual(keysAndValues({1: null, 2: null, 3: null}), [["1", "2", "3"], [null, null, null]]);
+    assert.deepEqual(keysAndValues({key1: "cat", key2: "dog", key3: null}), [["key1", "key2", "key3"], ["cat", "dog", null]]);
   });
 });
