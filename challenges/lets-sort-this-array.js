f5387134add4cd67dd4b2695c67a246fcbe99b0a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-27 16:03:17 -0700

    Solve lets sort this array challenge

--- challenges/lets-sort-this-array.js
+++ challenges/lets-sort-this-array.js
@@ -16,7 +16,13 @@
 // N/A
 
 function ascDesNone(arr, str) {
-
+  if (str === 'Asc') {
+    return arr.sort((a, b) => a - b);
+  } else if (str === 'Des') {
+    return arr.sort((a, b) => b - a);
+  } else {
+    return arr;
+  }
 }
 
 module.exports = ascDesNone;
