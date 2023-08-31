cd63d3b8b4d1bab929650e83cadb4d7e0c68ebda
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-31 15:31:39 -0700

    Create test for the decimator challenge

---
+++ challenges/the-decimator.js
@@ -0,0 +1,28 @@
+// The DECIMATOR
+// Write a DECIMATOR function which takes a string and decimates it (i.e. 
+//   it removes the last 1/10 of the characters).
+
+// Always round up: if the string has 21 characters, 1/10 of the characters would 
+// be 2.1 characters, hence the DECIMATOR removes 3 characters. The DECIMATOR shows no mercy!
+
+// Examples
+// DECIMATOR("1234567890") ➞ "123456789"
+// // 10 characters, removed 1.
+
+// DECIMATOR("1234567890AB") ➞ "1234567890"
+// // 12 characters, removed 2.
+
+// DECIMATOR("123") ➞ "12"
+// // 3 characters, removed 1.
+
+// DECIMATOR("123456789012345678901") ➞ "123456789012345678"
+// // 21 characters, removed 3.
+
+// Notes
+// Make sure to remove characters from the end of the string.
+
+function decimator(str) {
+  
+}
+
+module.exports = decimator
---
+++ test/the-decimator-test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert
+var decimator = require('../challenges/the-decimator');
+
+describe('The decimator', function() {;
+
+  it('takes a string and removes the last 1/10th of its characters', function() {
+    assert.equal(decimator("1234567890"), "123456789");
+    assert.equal(decimator("1234567890AB"), "1234567890");
+    assert.equal(decimator("123"), "12");
+    assert.equal(decimator("123456789012345678901"), "123456789012345678");
+    assert.equal(decimator("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "ABCDEFGHIJKLMNOPQRSTUVW");
+    assert.equal(decimator("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst");
+    assert.equal(decimator("A"), "");
+    assert.equal(decimator(""), "");
+  });
+});
