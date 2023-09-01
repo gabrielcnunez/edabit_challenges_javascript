3904a4bddd138f1e509ab1db9c90ad26fdc880fb
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-01 14:55:06 -0700

    Solve seven boom challenge

--- challenges/seven-boom.js
+++ challenges/seven-boom.js
@@ -16,7 +16,13 @@
 // N/A
 
 function sevenBoom(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].toString().includes('7')) {
+      return 'Boom!';
+    }
+  }
 
+  return 'there is no 7 in the array';
 }
 
 module.exports = sevenBoom;
