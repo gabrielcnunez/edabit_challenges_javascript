b663879bebd1ad310e6f2a1844f09ee6f0ea4455
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-06 12:58:26 -0700

    Solve pandigital numbers challenge

--- challenges/pandigitial-numbers.js
+++ challenges/pandigitial-numbers.js
@@ -14,7 +14,13 @@
 // Think about the properties of a pandigital number when all duplicates are removed.
 
 function isPandigital(num) {
+  const digitsArray = Array.from(String(num));
+  
+  const uniqueDigitsArray = [...new Set(digitsArray.map(Number))];
 
+  const sortedNums = uniqueDigitsArray.sort((a, b) => a - b).join('');
+
+  return sortedNums == 0123456789;
 }
 
 module.exports = isPandigital;
