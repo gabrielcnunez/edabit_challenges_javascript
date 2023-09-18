d9917edd125a2178f0331d0116a62e1bd58883ae
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-17 18:26:16 -0700

    Solve ordered matrix challenge

--- challenges/ordered-matrix.js
+++ challenges/ordered-matrix.js
@@ -1,5 +1,5 @@
 // An Ordered Matrix
-// Create an ordered 2D array (matrix). A matrix is ordered if its (0, 0) element is 1, 
+// Create an ordered 2D matrixay (matrix). A matrix is ordered if its (0, 0) element is 1, 
 // its (0, 1) element is 2, and so on. Your function needs to create an a × b matrix. 
 // a is the first argument and b is the second.
 
@@ -23,7 +23,17 @@
 // a and b are integers.
 
 function orderedMatrix(a, b) {
+  let matrix = [];
+  let value = 1;
 
+  for (let i = 0; i < a; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < b; j++) {
+      matrix[i][j] = value++;
+    }
+  }
+
+  return matrix;
 }
 
 module.exports = orderedMatrix;
