855ace97edf37320f0d8f4e9d877eff04c8e75db
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-20 10:51:05 -0700

    Create test for most expensive jewelry challenge

---
+++ challenges/most-expensive-jewelry.js
@@ -0,0 +1,29 @@
+// Return the Most Expensive Piece of Jewellery
+// You go to a jewelry shop and try to find the most expensive piece of jewelry. 
+// You write down the name of each piece of jewelry and its price.
+
+// Create a function that gets the name of the piece of jewelry with the highest 
+// price and returns "The most expensive one is the {name of jewelry piece}".
+
+// Examples
+// mostExpensive ({
+//   "Diamond Earrings": 980,
+//   "Gold Watch": 250,
+//   "Pearl Necklace": 4650
+// }) ➞  "The most expensive one is the Pearl Necklace"
+
+// mostExpensive({
+//   "Silver Bracelet": 280,
+//   "Gemstone Earrings": 180,
+//   "Diamond Ring": 3500
+// }) ➞ "The most expensive one is the Diamond Ring"
+
+// Notes
+// There will always be at least one item in the object.
+// There will always be only one highest priced item (i.e. there will not be two items with the joint highest value).
+
+function mostExpensive(obj) {
+  ``
+}
+
+module.exports = mostExpensive
---
+++ test/most-expensive-jewelry-test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert
+var mostExpensive = require('../challenges/most-expensive-jewelry')
+
+describe('Return the Most Expensive Piece of Jewelry', function() {
+
+  it('takes object, returns the name of jewelry with highest price', function() {
+    assert.equal(mostExpensive({'Diamond Earrings': 980, 'Gold Watch': 250, 'Pearl Necklace': 4650}), "The most expensive one is the Pearl Necklace")
+    assert.equal(mostExpensive({'Silver Bracelet': 280,'Gemstone Earrings': 180, 'Diamond Ring': 3500}), "The most expensive one is the Diamond Ring")
+    assert.equal(mostExpensive({'Simple Gold Ring': 310, 'Silver Anklet': 110, 'Crystal Bracelet': 190}), "The most expensive one is the Simple Gold Ring")
+  })
+})
