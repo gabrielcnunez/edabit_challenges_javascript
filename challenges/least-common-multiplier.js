841005f468bc8dd542bbcfe6950c0ea33b951926
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-09-26 09:16:39 -0700

    Create test for least common multiplier challenge

---
+++ challenges/least-common-multiplier.js
@@ -0,0 +1,20 @@
+// GCD and LCM (Part 2)
+// Create a function that takes two numbers as arguments and return the LCM of the two numbers.
+
+// Examples
+// lcm(3, 5) ➞ 15
+
+// lcm(14, 28) ➞ 28
+
+// lcm(4, 6) ➞ 12
+
+// Notes
+// Don't forget to return the result.
+// You may want to use the GCD function to make this a little easier.
+// LCM stands for least common multiple, the smallest multiple of both integers.
+
+function lcm(a, b) {
+
+}
+
+module.exports = lcm
---
+++ test/least-common-multiplier-test.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert
+const lcm = require('../challenges/least-common-multiplier')
+
+describe('GCD and LCM', function() {
+
+  it('takes two numbers, returns least common multiplier', function() {
+    assert.equal(lcm(6, 10), 30)
+    assert.equal(lcm(30, 60), 60)
+    assert.equal(lcm(10000, 333), 3330000)
+    assert.equal(lcm(75, 135), 675)
+    assert.equal(lcm(102, 2), 102)
+  })
+})
