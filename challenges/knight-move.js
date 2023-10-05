5c605b451f1cc5463982ec363277daf37307b393
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-10-05 16:19:50 -0700

    Solve knight move challenge

--- challenges/knight-move.js
+++ challenges/knight-move.js
@@ -16,7 +16,32 @@
 // N/A
 
 function knightMove(startPos) {
-  
+  const letter = startPos[0]
+  const number = parseInt(startPos[1])
+  const possibleMoves = []
+
+  const moves = [
+    { row: 2, col: 1 },
+    { row: 2, col: -1 },
+    { row: -2, col: 1 },
+    { row: -2, col: -1 },
+    { row: 1, col: 2 },
+    { row: 1, col: -2 },
+    { row: -1, col: 2 },
+    { row: -1, col: -2 },
+  ]
+
+  for (const move of moves) {
+    const newRow = number + move.row
+    const newCol = letter.charCodeAt(0) - 'A'.charCodeAt(0) + move.col
+
+    if (newRow >= 1 && newRow <= 8 && newCol >= 0 && newCol <= 7) {
+      const newLetter = String.fromCharCode('A'.charCodeAt(0) + newCol);
+      possibleMoves.push(`${newLetter}${newRow}`);
+    }
+  }
+
+  return possibleMoves.sort().join(' - ')
 }
 
 module.exports = knightMove
